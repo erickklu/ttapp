a29ef61d1b24ea3f923e8cebac7e478044e58541
erick <ericktrejojohanlucio@gmail.com>
2024-01-30 11:37:38 -0500

    Actualización archivo server.mjs

---
+++ android/app/src/main/java/com/ttapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ttapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- server.mjs
+++ server.mjs
@@ -1,74 +1,67 @@
-import express from 'express';
-import cors from 'cors';
-import mysql from 'mysql';
-
+import fs from "fs";
+import express from "express";
+import cors from "cors";
+ 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  database: 'tt_biblio',
-  user: 'root',
-  password: '',
-});
-
-connection.connect((err) => {
+// Leer el archivo JSON
+fs.readFile("data/estudiantes.json", "utf8", (err, data) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log('Conectado a la base de datos');
+    return;
   }
-});
 
-app.get('/api/validar-cedula/:cedula', (req, res) => {
-  const cedula = req.params.cedula;
-  const query = 'SELECT * FROM estudiante WHERE es_cedu = ?';
-  connection.query(query, [cedula], (error, results) => {
-    if (error) {
-      res.status(500).json({ error: 'Error al validar la cédula' });
+  const obj = JSON.parse(data);
+
+  app.get("/api/validar-cedula/:cedula", (req, res) => {
+    const cedula = req.params.cedula;
+
+    // Convertir la cédula a un número
+    const cedulaNum = Number(cedula);
+
+    const results = obj.estudiantes.filter(
+      (estudiante) => estudiante.es_cedu === cedulaNum
+    );
+    if (results.length > 0) {
+      const estudiante = {
+        apellidos: results[0].es_apel,
+        nombres: results[0].es_nomb,
+        cedula: results[0].es_cedu,
+      };
+
+      // Si la cédula ingresada existe en el archivo JSON
+      res.json({ valid: true, data: estudiante });
     } else {
-      if (results.length > 0) {
-        const estudiante = {
-          apellidos: results[0].es_ape,
-          nombres: results[0].es_nom,
-          cedula: results[0].es_cedu,
-        };
-        
-        // La cédula existe en la base de datos
-        res.json({ valid: true, data: estudiante });
-      } else {
-        // La cédula no existe en la base de datos
-        res.json({ valid: false, data: null });
-      }
+      // Si la cédula ingresada no existe en el archivo JSON
+      res.json({ valid: false, data: null });
     }
   });
-});
 
-app.get('/api/obtener-datos-estudiante/:cedula', (req, res) => {
-  const cedula = req.params.cedula;
-  const query = 'SELECT * FROM estudiante WHERE es_cedu = ?';
-  connection.query(query, [cedula], (error, results) => {
-    if (error) {
-      res.status(500).json({ error: 'Error al obtener datos del estudiante' });
+  
+  app.get("/api/obtener-datos-estudiante/:cedula", (req, res) => {
+    const cedula = req.params.cedula;
+
+    // Convertir la cédula a un número
+    const cedulaNum = Number(cedula);
+
+    const results = obj.estudiantes.filter(
+      (estudiante) => estudiante.es_cedu === cedulaNum
+    );
+    if (results.length > 0) {
+
+      // Si el número de cédula fue encontrado en el archivo JSON
+      const estudiante = results[0];
+      res.json({ estudiante });
     } else {
-      if (results.length > 0) {
-
-        // Si el estudiante fue encontrado en la base de datos
-        const estudiante = results[0];
-        res.json({ estudiante });
-
-      } else {
-        
-        // Si el estudiante no es encontrado en la base de datos
-        res.status(404).json({ error: 'Estudiante no encontrado' });
-      }
+      // Si el número de cédula no fue encontrado en el archivo JSON
+      res.status(404).json({ error: "Estudiante no encontrado" });
     }
   });
-});
 
-app.listen(port, () => {
-  console.log(`Backend running on http://localhost:${port}`);
+  app.listen(port, () => {
+    console.log(`Backend running on http://192.168.1.3:${port}`);
+  });
 });
